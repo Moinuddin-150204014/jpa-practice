72fa46cc5089ae84696fcfd068b6c267da1bce94
Moinuddin-150204014 <141901227+Moinuddin-150204014@users.noreply.github.com>
2023-09-07 17:13:10 +0600

    Changed

---
+++ securityPractice/src/main/java/com/spring/securityPractice/security/CustomAuthenticationFilter.java
@@ -0,0 +1,78 @@
+package com.spring.securityPractice.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring.securityPractice.SpringApplicationContext;
+import com.spring.securityPractice.constants.AppConstants;
+import com.spring.securityPractice.model.UserDto;
+import com.spring.securityPractice.model.UserLoginRequestModel;
+import com.spring.securityPractice.service.UserService;
+import com.spring.securityPractice.utils.JWTUtils;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Slf4j
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            UserLoginRequestModel creds = new ObjectMapper().readValue(request.getInputStream(), UserLoginRequestModel.class);
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword())
+            );
+        } catch (IOException e) {
+            log.info("Exception occurred at attemptAuthentication method: {}", e.getLocalizedMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String user = ((User) authResult.getPrincipal()).getUsername();
+        String accessToken = JWTUtils.generateToken(user);
+
+        // Create a response JSON containing the access token
+        String jsonResponse = "{\"accessToken\":\"" + accessToken + "\"}";
+
+        response.setContentType("application/json");
+        response.getWriter().write(jsonResponse);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+
+        String errorMessage;
+        if (failed instanceof BadCredentialsException) {
+            errorMessage = "Invalid username or password";
+        } else if (failed instanceof LockedException) {
+            errorMessage = "Account is locked";
+        } else {
+            errorMessage = "Authentication failed";
+        }
+
+        String jsonResponse = "{\"message\":\"" + errorMessage + "\"}";
+        response.getWriter().write(jsonResponse);
+    }
+
+
+}
